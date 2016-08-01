59b1324efc20e807375f983237eea4613162b4da
ccasbre27@hotmail.com <ccasbre27@hotmail.com>
2016-08-01 07:03:04 -0600

    Initial Commit

---
+++ TestWifi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestWifi
+//
+//  Created by user on 8/1/16.
+//  Copyright © 2016 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestWifi/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestWifi
+//
+//  Created by user on 8/1/16.
+//  Copyright © 2016 user. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
